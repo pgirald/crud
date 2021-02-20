70bd7269c69cab6e7f60125a9176b1a96dfe48db
pgirald <79157806+pgirald@users.noreply.github.com>
2021-02-20 18:39:04 -0500

    "Task add" functionality added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-import react from 'react';
+import react,{useState} from 'react';
+import {isEmpty} from 'lodash'
+import shortid from 'shortid';
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  function onTextChangedhandler(text)
+  {
+    return setTask(text.target.value);
+  }
+
+  function addTask(e)
+  {
+    e.preventDefault();
+    if(isEmpty(task))
+    {
+      console.log("Task empty");
+      return;
+    }
+    const newTask={
+      id:shortid.generate(),
+      name:task};
+    setTasks([...tasks,newTask]);
+    setTask("");
+  }
+
+  function removeTask(e)
+  {
+  }
+
+  function modifyTask(e)
+  {}
   return (<div className="container mt-5">
     <h1>Tareas</h1>
     <hr/>
+    <div className="row">
+      <div className="col-8">
+        <h4 className="text-center">Lista de tareas</h4>
+        <ul className="list-groud">
+          {
+            tasks.map(task=>(
+            <li className="list-group-item" key={task.id}>
+              <span className="lead">{task.name}</span>
+              <button className="btn btn-danger btn-sm float-right mx-2" onClick={removeTask}>Eliminar</button>
+              <button className="btn btn-warning btn-sm float-right" onClick={modifyTask}>Editar</button>
+            </li>))
+          }
+        </ul>
+      </div>
+      <div className="col-4">
+        <h4 className="text-center">formularios</h4>
+        <form onSubmit={addTask}>
+          <input type="text" className="form-control mb-2" placeholder="ingrese la tarea:"
+          onChange={onTextChangedhandler} value={task}/>
+          <button type="submit" className="btn btn-dark btn-block">Agregar</button>
+        </form>
+      </div>
+    </div>
   </div>
   );
 }
