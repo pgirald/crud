ed17858a4764ddef61820cc3f691947823343db8
pgirald <79157806+pgirald@users.noreply.github.com>
2021-02-20 19:31:01 -0500

    Edit and delete funcs added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import react,{useState} from 'react';
-import {isEmpty} from 'lodash'
+import {isEmpty,size} from 'lodash'
 import shortid from 'shortid';
 
 function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [editMode,setEditMode]=useState(false);
+  const [id,setId]=useState("");
+
   function onTextChangedhandler(text)
   {
     return setTask(text.target.value);
@@ -25,35 +28,64 @@ function App() {
     setTask("");
   }
 
-  function removeTask(e)
+  function removeTask(id)
   {
+    console.log(id);
+    const newTasks=tasks.filter(task=>task.id!==id);
+    setTasks(newTasks);
   }
 
-  function modifyTask(e)
-  {}
+  function modifyTask(theTask)
+  {
+    setTask(theTask.name);
+    setEditMode(true);
+    setId(theTask.id);
+  }
+
+  function saveTaskChanges(e)
+  {
+    e.preventDefault();
+    if(isEmpty(task))
+    {
+      console.log("Task empty");
+      return;
+    }
+    setEditMode(false);
+    const selectedTask=tasks.find(task=>task.id==id);
+    selectedTask.name=task;
+    setTask("");
+    setId("");
+  }
+
   return (<div className="container mt-5">
     <h1>Tareas</h1>
     <hr/>
     <div className="row">
       <div className="col-8">
         <h4 className="text-center">Lista de tareas</h4>
-        <ul className="list-groud">
-          {
-            tasks.map(task=>(
-            <li className="list-group-item" key={task.id}>
-              <span className="lead">{task.name}</span>
-              <button className="btn btn-danger btn-sm float-right mx-2" onClick={removeTask}>Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right" onClick={modifyTask}>Editar</button>
-            </li>))
-          }
-        </ul>
+        {
+          (size(tasks)===0)?(<h5 className="text-center">Aun no hay tareas programadas</h5>):(
+          <ul className="list-groud">
+            {
+              tasks.map(task=>(
+              <li className="list-group-item" key={task.id}>
+                <span className="lead">{task.name}</span>
+                <button className="btn btn-danger btn-sm float-right mx-2" onClick={()=>removeTask(task.id)}>Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right" onClick={()=>modifyTask(task)}>Editar</button>
+              </li>))
+            }
+          </ul>
+          )
+        }
       </div>
       <div className="col-4">
-        <h4 className="text-center">formularios</h4>
-        <form onSubmit={addTask}>
+        <h4 className="text-center">Nombre de la tarea</h4>
+        <form onSubmit={editMode?saveTaskChanges:addTask}>
           <input type="text" className="form-control mb-2" placeholder="ingrese la tarea:"
           onChange={onTextChangedhandler} value={task}/>
-          <button type="submit" className="btn btn-dark btn-block">Agregar</button>
+          <button type="submit" className="btn btn-dark btn-block">
+            {editMode?"Modificar":"Agregar"}
+            </button>
         </form>
       </div>
     </div>
